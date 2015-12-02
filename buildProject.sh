b74d0cd6c9b87b61196038661519c4651d1ba0ac
tecris <eugen@cristea.org>
2015-12-02 22:41:18 +1300

    update

--- buildProject.sh
+++ buildProject.sh
@@ -1,11 +1,10 @@
 # --topLevelPackage - maven groupId
 # --named - maven artifactId
 project-new --named planets --topLevelPackage org.planets --finalName planets --type war
-scaffold-setup
 jpa-setup
 jpa-new-entity --named Planet
 jpa-new-field --named name
 jpa-new-field --named galaxy
-#jpa-new-field --named age --type int
+scaffold-setup --provider AngularJS
 scaffold-generate --provider AngularJS --targets org.planets.model.Planet
 rest-generate-endpoints-from-entities --targets org.planets.model.Planet
