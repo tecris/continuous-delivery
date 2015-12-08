8b13db1c168f633f7696be7d6ee6d38bfc124ab3
tecris <eugen@cristea.org>
2015-12-08 13:44:01 +1300

    use object to generate json

--- src/test/java/org/planets/rest/PlanetEndpointIT.java
+++ src/test/java/org/planets/rest/PlanetEndpointIT.java
@@ -5,19 +5,27 @@ import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Test;
+import org.planets.model.Planet;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.restassured.RestAssured;
 
 public class PlanetEndpointIT {
 
 	@Test
-	public void postExample() {
+	public void postExample() throws JsonProcessingException {
+
 		String expectedName = "Mercury";
 		String expectedGalaxy = "Milky Way";
-		String myJson = "{\"name\":\""+expectedName+"\",\"galaxy\":\""+expectedGalaxy+"\"}";
+		ObjectMapper mapper = new ObjectMapper();
+		Planet planet = new Planet();
+		planet.setGalaxy(expectedGalaxy);
+		planet.setName(expectedName);
+		String jsonInString = mapper.writeValueAsString(planet);
 		RestAssured.baseURI = "http://127.0.0.1:8080/planets/rest/planets";
 
-		given().contentType("application/json").body(myJson).when().post("");
+		given().contentType("application/json").body(jsonInString).when().post("");
 		
 		get("/1").then().
 		body("name", equalTo(expectedName)).
