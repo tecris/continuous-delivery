017e989d536ed42749494c2047a7179ffb8340ef
Eugen Cristea <eugen@cristea.org>
2015-12-17 10:05:03 +1300

    renamed file

--- pre_requisites.sh
+++ pre_requisites.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --branch v3.3 https://github.com/tecris/docker.git
+git clone --branch v3.5 https://github.com/tecris/docker.git
 docker build --no-cache -t casa.docker/jdk:8 docker/jdk/8
 docker build --no-cache -t casa.docker/wildfly:9.0.2 docker/wildfly/9
 docker build --no-cache -t casa.docker/wildfly-mysql:9.0.2 docker/wildfly-mysql
@@ -8,5 +8,6 @@ docker build --no-cache -t casa.docker/mysql:5.7 docker/mysql/57
 docker build --no-cache -t casa.docker/wildfly-postgres:9.0.2 docker/wildfly-postgres
 docker build --no-cache -t casa.docker/postgres:9.4 docker/postgres/94
 docker build --no-cache -t casa.docker/nexus::2.11.4-01 docker/nexus
+rm -rf docker
 # start maven nexus repository
 docker run --name nexus --restart=always -d -p 172.17.0.1:8081:8081 -v /opt/docker_volumes/nexus-data:/sonatype-work casa.docker/nexus:2.11.4-01
