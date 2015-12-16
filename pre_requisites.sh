1f803212371a02d26ea202a413781b918fc6d6b2
Eugen Cristea <eugen@cristea.org>
2015-12-17 11:16:51 +1300

    typo

--- pre_requisites.sh
+++ pre_requisites.sh
@@ -7,7 +7,7 @@ docker build --no-cache -t casa.docker/wildfly-mysql:9.0.2 docker/wildfly-mysql
 docker build --no-cache -t casa.docker/mysql:5.7 docker/mysql/57
 docker build --no-cache -t casa.docker/wildfly-postgres:9.0.2 docker/wildfly-postgres
 docker build --no-cache -t casa.docker/postgres:9.4 docker/postgres/94
-docker build --no-cache -t casa.docker/nexus::2.11.4-01 docker/nexus
+docker build --no-cache -t casa.docker/nexus:2.11.4-01 docker/nexus
 rm -rf docker
 # start maven nexus repository
 docker run --name nexus --restart=always -d -p 172.17.0.1:8081:8081 -v /opt/docker_volumes/nexus-data:/sonatype-work casa.docker/nexus:2.11.4-01
