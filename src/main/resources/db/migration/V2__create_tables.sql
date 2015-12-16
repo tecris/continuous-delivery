1642242919fbc16ad579bcf4cd90e1891fb903c9
Eugen Cristea <eugen@cristea.org>
2015-12-16 17:06:46 +1300

    postgres preparation

--- src/main/resources/db/migration/V2__create_tables.sql
+++ src/main/resources/db/migration/V2__create_tables.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `Planet` (
-  `id` bigint(20) NOT NULL,
-  `Galaxy` varchar(255) DEFAULT NULL,
-  `Name` varchar(255) DEFAULT NULL,
-  `version` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE Planet (
+  id numeric(20) NOT NULL,
+  Galaxy varchar(255) DEFAULT NULL,
+  Name varchar(255) DEFAULT NULL,
+  version int(11) DEFAULT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE TABLE_GENERATOR
