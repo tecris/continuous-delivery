7232f462cedd34ab0495bc0292951058d7034910
Eugen Cristea <eugen@cristea.org>
2015-12-16 17:18:35 +1300

    update for postgres

--- src/main/resources/db/migration/V2__create_tables.sql
+++ src/main/resources/db/migration/V2__create_tables.sql
@@ -2,14 +2,14 @@ CREATE TABLE Planet (
   id numeric(20) NOT NULL,
   Galaxy varchar(255) DEFAULT NULL,
   Name varchar(255) DEFAULT NULL,
-  version int(11) DEFAULT NULL,
+  version integer DEFAULT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE TABLE_GENERATOR
 (
   gen_key   varchar(255) not null,
-  gen_value int(11),
+  gen_value integer,
   PRIMARY KEY (gen_key)
 );
 INSERT INTO TABLE_GENERATOR (Gen_Key,GEN_VALUE) VALUES ('PLANETS.GENERATOR',0);
