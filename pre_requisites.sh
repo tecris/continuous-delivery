d2943797f3073e639ceb39f1eb33d0893919237b
Eugen Cristea <eugen@cristea.org>
2015-12-19 10:13:56 +1300

    Update pre_requisites.sh

--- pre_requisites.sh
+++ pre_requisites.sh
@@ -10,4 +10,7 @@ docker build --no-cache -t casa.docker/postgres:9.4 docker/postgres/94
 docker build --no-cache -t casa.docker/nexus:2.11.4-01 docker/nexus
 rm -rf docker
 # start maven nexus repository
-docker run --name nexus --restart=always -d -p 172.17.0.1:8081:8081 -v /opt/docker_volumes/nexus-data:/sonatype-work casa.docker/nexus:2.11.4-01
+docker run --name nexus --restart=always -d -p 172.17.0.1:8081:8081  casa.docker/nexus:2.11.4-01
+# nexus with docker volume
+# mkdir /opt/docker_volumes/nexus-data && chown -R 200 /opt/docker_volumes/nexus-data
+# docker run --name nexus --restart=always -d -p 172.17.0.1:8081:8081 -v /opt/docker_volumes/nexus-data:/sonatype-work casa.docker/nexus:2.11.4-01
