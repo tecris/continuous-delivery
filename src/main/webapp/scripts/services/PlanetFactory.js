e8ffaa1f6991598ba379f5e8e44af948d96dc9d6
Eugen Cristea <eugen@cristea.org>
2015-12-22 08:29:56 +1300

    url path fix

--- src/main/webapp/scripts/services/PlanetFactory.js
+++ src/main/webapp/scripts/services/PlanetFactory.js
@@ -1,4 +1,4 @@
 angular.module('planets').factory('PlanetResource', function($resource){
-    var resource = $resource('rest/planets/:PlanetId',{PlanetId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
+    var resource = $resource('rest/planet/:PlanetId',{PlanetId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
     return resource;
 });
