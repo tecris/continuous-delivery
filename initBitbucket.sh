534894109addf43b4d59448ce65826653b9d39a9
Eugen Cristea <eugen@cristea.org>
2016-02-01 09:27:22 +0100

    update

--- initBitbucket.sh
+++ initBitbucket.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/tecris/backup/v16.01.29/configureBitbucket.sh
-chmod 744 configureBitbucket.sh
+wget https://raw.githubusercontent.com/tecris/backup/v16.02.01/configureBitbucket.sh
 ./configureBitbucket.sh
 rm configureBitbucket.sh
