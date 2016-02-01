2a03859e9fe6f36c5ccedf55cfded8fec1de634e
Eugen Cristea <eugen@cristea.org>
2016-02-01 09:58:25 +0100

    nexus volume fix for the very first start

--- startCdInfrastructure.sh
+++ startCdInfrastructure.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo mkdir -p /opt/docker_volumes/nexus-data && sudo chown -R 200 /opt/docker_volumes/nexus-data
+
 docker-compose -f continuous-delivery.yml up -d sonar-db
 docker-compose -f continuous-delivery.yml up -d nexus sonar jenkins
