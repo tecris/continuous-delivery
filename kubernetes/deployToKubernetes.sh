dfb0483bf5e96fa626cdae0cfc6d08f1c9d691e3
Eugen Cristea <eugen@cristea.org>
2016-03-22 18:17:57 +0200

    git rm -rf kubernetes

--- kubernetes/deployToKubernetes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl create -f kubernetes/planets-db-pod.yaml
-kubectl create -f kubernetes/planets-db-svc.yaml
-kubectl create -f kubernetes/planets-web-rc-v1.yaml
-kubectl create -f kubernetes/planets-web-svc.yaml
