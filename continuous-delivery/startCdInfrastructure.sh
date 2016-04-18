2e84fb300ad59eb5570e36e14912f18113566071
Eugen Cristea <eugen@cristea.org>
2016-04-18 18:35:08 +0300

    use depends_on to control container startup order

--- continuous-delivery/startCdInfrastructure.sh
+++ continuous-delivery/startCdInfrastructure.sh
@@ -5,5 +5,4 @@ sudo mkdir -p /opt/docker_volumes/nexus-data && sudo chown -R 200 /opt/docker_vo
 export DOCKER_IP=`ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1`
 export DOCKER0_IP=`ip -o -4 addr list docker0 | awk '{print $4}' | cut -d/ -f1`
 
-docker-compose -f continuous-delivery.yml up -d sonar-db
-docker-compose -f continuous-delivery.yml up -d nexus sonar jenkins
+docker-compose -f continuous-delivery.yml up -d 
