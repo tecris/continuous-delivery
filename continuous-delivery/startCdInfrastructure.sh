efb5adb5a28cabfa947caa6ea1536f8a16be19bb
Eugen Cristea <eugen@cristea.org>
2016-04-24 15:11:22 +0300

    update

--- continuous-delivery/startCdInfrastructure.sh
+++ continuous-delivery/startCdInfrastructure.sh
@@ -5,4 +5,5 @@ sudo mkdir -p /opt/docker_volumes/nexus-data && sudo chown -R 200 /opt/docker_vo
 export DOCKER_IP=`ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1`
 export DOCKER0_IP=`ip -o -4 addr list docker0 | awk '{print $4}' | cut -d/ -f1`
 
+# docker-compose -f continuous-delivery-light.yml up -d
 docker-compose -f continuous-delivery.yml up -d 
