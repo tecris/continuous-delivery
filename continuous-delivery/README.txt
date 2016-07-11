fe16099813da943c258590f78556ae36bbc1c26e
Eugen Cristea <eugen@cristea.org>
2016-07-11 17:44:56 +0300

    git rm -rf continuous-delivery

--- continuous-delivery/README.txt
+++
@@ -1,6 +0,0 @@
-Components:
-
- .) Jenkins - continuous integration/delivery server
- .) Nexus - maven repository
- .) Docker private registry
- .) Sonarqube - quality management control
--- continuous-delivery/startCdInfrastructure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo mkdir -p /opt/docker_volumes/nexus-data && sudo chown -R 200 /opt/docker_volumes/nexus-data
-
-export DOCKER_IP=`ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1`
-export DOCKER0_IP=`ip -o -4 addr list docker0 | awk '{print $4}' | cut -d/ -f1`
-
-# docker-compose -f continuous-delivery-light.yml up -d
-docker-compose -f continuous-delivery.yml up -d 
