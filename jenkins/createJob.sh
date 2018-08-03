f818854cf797aef7994aa1fd7ad5c6d8da6b5909
Eugen Cristea <eugen@cristea.org>
2018-08-03 09:19:39 +0200

    jenkins crumb [ci skip]

--- jenkins/createJob.sh
+++ jenkins/createJob.sh
@@ -9,6 +9,9 @@ PORT=$2
 JOB_NAME=$3
 FILE=$4
 
+CRUMB=$(wget -q --auth-no-challenge --user $USER --password $PASSWORD --output-document - \
+         "http://${HOST}:${PORT}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
+
 curl -H "Content-Type: application/xml" \
         -H "$CRUMB" \
         -X POST "http://$HOST:$PORT/createItem?name=$JOB_NAME&token=$TOKEN" \
