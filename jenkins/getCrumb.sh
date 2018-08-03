4aee70b51a9dabffc01052f6b4baadfcefede638
Eugen Cristea <eugen@cristea.org>
2018-08-03 09:10:02 +0200

    jenkins crumb [ci skip]

--- jenkins/getCrumb.sh
+++ jenkins/getCrumb.sh
@@ -4,7 +4,9 @@
 
 # https://wiki.jenkins.io/display/JENKINS/Remote+access+API#RemoteaccessAPI-CSRFProtection
 
+HOST=localhost
+
 CRUMB=$(wget -q --auth-no-challenge --user $USER --password $PASSWORD --output-document - \
-         'http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+         "http://${HOST}:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
 
 echo $CRUMB
